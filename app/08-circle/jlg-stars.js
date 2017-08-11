2bfc98a73815e0408d04a40a4afcb68da69d1923
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-08-11 18:43:04 +0200

    add: onclick

--- app/08-circle/jlg-stars.js
+++ app/08-circle/jlg-stars.js
@@ -27,22 +27,15 @@
 </style>
 			`;
 			for (let i = 0; i < note; i++) {
-				html += `<img onmouseover="console.log('%O', this)" src="img/yellow_star.png">`;
+				html += `<img onclick="this.parentNode.host.update(${i+1})" src="img/yellow_star.png">`;
 			}
 
 			for (let i = note; i < 5; i++) {
-				html += `<img src="img/white_star.png">`;
+				html += `<img onclick="this.parentNode.host.update(${i+1})" src="img/white_star.png">`;
 			}
 
 			this.root.innerHTML = html;
-			const images = this.root.querySelectorAll('img');
-			console.log('images', images);
-			const self = this;
-			images.forEach((img, i) => {
-				img.addEventListener('click', function () {
-					self.update(i + 1);
-				})
-			});
+			
 		}
 
 		hello() {

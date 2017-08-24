e1d180f0aa036cd852d273028e3f052654a85892
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-08-24 14:25:31 +0200

    add: notation []

--- app/19-deep-observable/app.js
+++ app/19-deep-observable/app.js
@@ -8,28 +8,32 @@
 
 	const object = {};
 
-	const handler = {
-		set(target, key, value) {
-			if (Array.isArray(target) && key === 'length') {
+	function handler(parentKey) {
+		return {
+			set(target, key, value) {
+				const absoluteKey = (parentKey) ? `${parentKey}['${key}']` : key;
+				if (Array.isArray(target)) {
+					if (key === 'length') {
+						return true;
+					}
+				}
+				console.log(`Setting value ${absoluteKey} as ${value}`, stackTrace());
+				if (value !== null && typeof value === 'object') {
+					target[key] = new Proxy(value, handler(absoluteKey));
+				} else {
+					target[key] = value;
+				}
+				return true;
+			},
+	
+			deleteProperty(target, key) {
+				console.log(`Deleting value ${key}`, stackTrace());
 				return true;
 			}
-			console.log(`Setting value ${key} as ${value}`, stackTrace());
-			if (value !== null && typeof value === 'object') {
-				target[key] = new Proxy(value, handler);
-			} else {
-				target[key] = value;
-			}
-			
-			return true;
-		},
+		};
+	}
 
-		deleteProperty(target, key) {
-			console.log(`Deleting value ${key}`, stackTrace());
-			return true;
-		}
-	};
-
-    const model = new Proxy(object, handler);
+    const model = new Proxy(object, handler());
     
 	model.hello = 'world';
 	model.obj = { hello: 'world'};

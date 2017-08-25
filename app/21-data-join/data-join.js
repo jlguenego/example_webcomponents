ac7b6d5b97e3342f32e163df36af158f56b04ee9
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-08-25 17:13:54 +0200

    add: data join

--- app/21-data-join/data-join.js
+++ app/21-data-join/data-join.js
@@ -1,69 +1,87 @@
 (function() {
 	'use strict';
 
-	const a1 = ['A', 'C', 'D', 'U', 'V', 'C', 'D', 'Z'];
-	const a2 = ['B', 'C', 'D', 'X', 'D', 'V', 'U', 'D'];
+	const a1 = ['A', 'B', 'C'];
+	const a2 = ['C', 'B', 'D', 'Z'];
 
-	
+	function insertAfter(newNode, referenceNode) {
+		referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+	}
+
+
 
 	class DJ {
 
-		constructor(selector, template, data) {
+		constructor(selector, template) {
 			this.element = document.querySelector(selector);
 			this.template = document.querySelector(template);
 			if (!this.element || !this.template) {
 				throw new Error('cannot find the selector: %s', selector);
 			}
+			this.data = [];
 		}
 
 		update(array) {
-            // a1 -> intersect -> a2
-            const a1 = this.element.querySelectorAll('[name="dj"]');
-            for (let i = 0; i < a1.length; i++) {
-                const item = a1[i].$data$.item;
-                if (!a2.includes(item)) {
-                    this.exit(a1[i]).then(() => {
-                        this.element.removeChild(a1[i]);
-                    });
-                    
-                }
-            }
 
-			array.forEach((item, index) => {
-                const elt = document.importNode(this.template.content, true);
-                console.log('elt %O', elt);
-                elt.querySelector('[name="letter"]').innerHTML = item;
-                elt.querySelector('[name="dj"]').$data$ = {item, index};
-                this.element.appendChild(elt);
+			// 1) remove
+            const elts = this.element.querySelectorAll('[name="dj"]');
+            let lastIndex = -1;
+            const intersect = [];
+			for (let i = 0; i < elts.length; i++) {
+				const item = elts[i].$data$;
+				const index = array.indexOf(item);
+				if (index <= lastIndex) {
+					this.exit(elts[i]).then(() => {
+						this.element.removeChild(elts[i]);
+					});
+				} else {
+					intersect.push({ item, index });
+                }
+                lastIndex = index;
+            }
+            console.log('intersect', intersect);
+
+			const newItems = array.filter(n => { 
+                return !intersect.find(x => x.item === n);
             });
-            return this;
-        }
-        onExit(promise) {
-            this.exit = promise;
-            return this;
-        }
+			console.log('newItems', newItems);
+
+			newItems.forEach((item, index) => {
+				const elt = document.importNode(this.template.content, true);
+				console.log('elt %O', elt);
+				elt.querySelector('[name="letter"]').innerHTML = item;
+				elt.querySelector('[name="dj"]').$data$ = item;
+				this.element.appendChild(elt);
+			});
+			this.data = array;
+			return this;
+		}
+		onExit(promise) {
+			this.exit = promise;
+			return this;
+		}
 	}
 
 
 
 	window.DJ = DJ;
 
-    const dj = new DJ('my-work-area', '#cities');
-    dj.onExit(function(elt) {
-        return new Promise((fulfill, reject) => {
-            console.log('about to exit in 2s', elt.$data$);
-            elt.className += 'leaving';
-            setTimeout(() => {
-                console.log('exit');
-                fulfill();
-            }, 5000);
-        });
-    });
-    dj.update(a1);
-    console.log('dj', dj);
-    setTimeout(() => {
-        dj.update(a2);
-    }, 3000);
-	
+	const dj = new DJ('my-work-area', '#cities');
+	dj.onExit(function(elt) {
+		return new Promise((fulfill, reject) => {
+			console.log('about to exit in 2s', elt.$data$);
+			elt.className += 'leaving';
+			setTimeout(() => {
+				console.log('exit');
+				fulfill();
+			}, 4000);
+		});
+	});
+	dj.update(a1);
+	console.log('dj', dj);
+	setTimeout(() => {
+		dj.update(a2);
+	}, 3000);
+
 
 })();

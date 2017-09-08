0e5c9701cbbe205a0bdb4c93eae6c5772d6e99c2
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-09-08 10:17:33 +0200

    o-if refactor with slot

--- app/circle/o-if.js
+++ app/circle/o-if.js
@@ -1,50 +1,10 @@
 (function() {
 	'use strict';
 
-	// Unfortunately, custom built-in element are not yet implemented.
-
 	class OIf extends circle.Element {
-
-		constructor() {
-			super();
-			console.log('o-if constructor');
-			this.oifChildNodes = [];
-		}
-
-		connectedCallback() {
-			super.connectedCallback();
-			// o-if
-			if (this.hasAttribute('tmpl-selector')) {
-				const originalTemplate = this.myDoc.querySelector(this.getAttribute('tmpl-selector'));
-				if (originalTemplate) {
-					this.originalContent = document.importNode(originalTemplate.content, true);
-				}
-			} else {
-				const originalTemplate = this.querySelector('template');
-				if (originalTemplate) {
-					this.originalContent = document.importNode(originalTemplate.content, true);
-				}
-			}
-		}
-
 		render(digestId) {
-			if (this.model.cond) {
-				this.root.innerHTML = '';
-				if (this.oifChildNodes.length === 0) {
-					this.root.appendChild(this.originalContent);					
-				} else {
-					while (this.oifChildNodes.length > 0) {
-						this.root.appendChild(this.oifChildNodes.shift());
-					}
-				}
-			} else {
-				console.log('render off', this.root);
-				while (this.root.firstChild) {
-					this.oifChildNodes.push(this.root.removeChild(this.root.firstChild));
-				}
-			}
+			this.root.innerHTML = (this.model.cond) ? '<slot></slot>' : '';
 		}
 	}
-
 	OIf.register();
 })();

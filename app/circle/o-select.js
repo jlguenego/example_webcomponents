22ece2eaf95fe85bff56203bf90f4767e1481d19
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-09-09 19:25:03 +0200

    add: o-select

--- app/circle/o-select.js
+++ app/circle/o-select.js
@@ -6,8 +6,17 @@
 		render(digestId) {
 			console.log('o-select render');
 			this.root.innerHTML = '<slot></slot>';
-			const select = this.root.querySelector('select');
-			console.log('select', select);
+			const select = this.querySelector('select');
+			console.log('select %O', select);
+			const value = select.selectedOptions[0].value;
+			console.log('value', value);
+			if (this.model.value !== value) {
+				this.update(value);
+			}
+		}
+
+		update(value) {
+			this.model.value = value;
 		}
 	}
 	OSelect.register();

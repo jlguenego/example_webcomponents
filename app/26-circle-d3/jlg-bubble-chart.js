e66586d2db827c9f179ddf07ae2e5d8eceb0ff8d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-09-09 18:19:33 +0200

    add: ciqual

--- app/26-circle-d3/jlg-bubble-chart.js
+++ app/26-circle-d3/jlg-bubble-chart.js
@@ -4,7 +4,7 @@
 	class JLGBubbleChart extends circle.Element {
 		render() {
 			this.root.innerHTML =
-				'<svg width="400" height="400" font-family="sans-serif" font-size="10" text-anchor="middle"></svg>';
+				'<svg width="600" height="600" font-family="sans-serif" font-size="10" text-anchor="middle"></svg>';
 			const svgNode = this.root.querySelector('svg');
 			const svg = d3.select(svgNode);
 			const width = +svg.attr('width');
@@ -18,30 +18,17 @@
 				.size([width, height])
 				.padding(1.5);
 
-			let csv = this.model.csv;
-			if (csv === '') {
-				return;
-			}
-
-			const data = d3.csvParse(csv, function(d) {
-				d.value = +d.value;
-				return d;
-			});
-
-			const root = d3.hierarchy({ children: data })
+			const root = d3.hierarchy({ children: this.model.data })
 				.sum(function(d) { return d.value; })
 				.each(function(d) {
-					var id;
-					if ((id = d.data.id)) {
-						var i = id.lastIndexOf('.');
-						d.id = id;
-						d.package = id.slice(0, i);
-						d.class = id.slice(i + 1);
-					}
+					d.id = d.data.id;
 				});
 
+			const bubbleData = pack(root).leaves();
+			console.log('bubbleData', bubbleData);
+
 			const node = svg.selectAll('.node')
-				.data(pack(root).leaves())
+				.data(bubbleData)
 				.enter().append('g')
 				.attr('class', 'node')
 				.attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; });
@@ -49,7 +36,7 @@
 			node.append('circle')
 				.attr('id', function(d) { return d.id; })
 				.attr('r', function(d) { return d.r; })
-				.style('fill', function(d) { return color(d.package); });
+				.style('fill', function(d) { return color(d.id); });
 
 			node.append('clipPath')
 				.attr('id', function(d) { return 'clip-' + d.id; })
@@ -59,14 +46,17 @@
 			node.append('text')
 				.attr('clip-path', function(d) { return 'url(#clip-' + d.id + ')'; })
 				.selectAll('tspan')
-				.data(function(d) { return d.class.split(/(?=[A-Z][^A-Z])/g); })
-				.enter().append('tspan')
+				.data(function(d) { 
+					const result = d.data.labels; 
+					console.log('result', result);
+					return result;
+				}).enter().append('tspan')
 				.attr('x', 0)
 				.attr('y', function(d, i, nodes) { return 13 + (i - nodes.length / 2 - 0.5) * 10; })
 				.text(function(d) { return d; });
 
 			node.append('title')
-				.text(function(d) { return d.id + '\n' + format(d.value); });
+				.text(function(d) { return d.data.tooltip; });
 
 		}
 	}
